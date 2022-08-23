b2da86d1a5804d5916d6fec084d1dcb32fbbc575
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-08-23 18:20:59 -0300

    Controle das áreas da tela

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Leonardo Carvalho',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'img1.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano de Tal',
+                partido: 'ABCD',
+                fotos: [
+                    { url: 'img2.png', legenda: 'Vereador', small: false }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 36,
+                nome: 'Leonardo Mercês',
+                partido: 'ABCED',
+                vice: 'Jéssica',
+                fotos: [
+                    { url: 'img3.png', legenda: 'Prefeito' },
+                    { url: 'img2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 25,
+                nome: 'Jéssica Lourenço',
+                partido: 'ABCDE',
+                vice: 'Leonardo',
+                fotos: [
+                    { url: 'img1.png', legenda: 'Prefeito' },
+                    { url: 'img3.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
+
