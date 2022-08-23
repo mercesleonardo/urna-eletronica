7a62017c4bd98d048e03f92f2550b8c74f9c81a8
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-08-23 19:03:39 -0300

    Preenchendo Interface com números

--- script.js
+++ script.js
@@ -4,3 +4,55 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = '';
+
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+function atualizaInterfaces() {
+    alert('FIM!');
+}
+
+function clicou(n) {
+    let elnumero = document.querySelector('.numero.pisca')
+    if (elnumero !== null) {
+        elnumero.innerHTML = n;
+        numero = `${numero} ${n}`;
+
+        elnumero.classList.remove('pisca');
+        if (elnumero.nextElementSibling !== null) {
+            elnumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterfaces();
+        }
+    }
+}
+function branco() {
+    alert('branco');
+}
+function corrige() {
+    alert('corrige');
+}
+function confirma() {
+    alert('confirma');
+}
+
+comecarEtapa();
